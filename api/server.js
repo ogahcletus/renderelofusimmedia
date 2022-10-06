4c5ef49a7f6da2bf6a1011c4743d84f94e7d8a88
Cletus Ogah <ogahcletus@gmail.com>
2022-10-06 23:26:41 +0100

    modify server & packagejson

--- api/server.js
+++ api/server.js
@@ -50,7 +50,7 @@ app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, 'client', 'build')));
+  app.use(express.static(path.join(__dirname, '/client/build')));
   
   app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
